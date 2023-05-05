f20719ac6ea547b9daa1d6223f4d1e3858bff569
Wyatt Ross <2treecko@gmail.com>
2023-05-05 11:57:24 -0400

    Images work. Just happens to look terrible rn lmao

--- src/components.rs
+++ src/components.rs
@@ -6,7 +6,7 @@ pub fn Navbar(cx: Scope) -> impl IntoView{
     view!{cx,
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <img src="resources/EzASM.png" alt="EzASM Logo"/>
+                <img src="public/EzASM.png" alt="EzASM Logo"/>
                 <a href="/" class="flex items-center">
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"EzASM"}</span>
                 </a>
