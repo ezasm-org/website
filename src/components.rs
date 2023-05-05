cd1ce0909681eb51a7de00e74f100d0a13abf22d
Wyatt Ross <2treecko@gmail.com>
2023-05-05 10:20:40 -0400

    trying, and mostly failing, to get images working

--- src/components.rs
+++ src/components.rs
@@ -6,6 +6,7 @@ pub fn Navbar(cx: Scope) -> impl IntoView{
     view!{cx,
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <img src="resources/EzASM.png" alt="EzASM Logo"/>
                 <a href="/" class="flex items-center">
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"EzASM"}</span>
                 </a>
@@ -16,11 +17,32 @@ pub fn Navbar(cx: Scope) -> impl IntoView{
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{"Home"}</a>
+                        <NavbarComponent href="#" text="Home"/>
+                    </li>
+                    <li>
+                        <NavbarComponent href="#" text="About"/>
+                    </li>
+                    <li>
+                        //TODO add a dropdown here
+                        <NavbarComponent href="#" text="Documentation"/>
+                    </li>
+                    <li>
+                        <NavbarComponent href="#" text="Download"/>
                     </li>
                 </ul>
             </div>
             </div>
         </nav>
+    }
+}
+
+#[component]
+pub fn NavbarComponent(cx: Scope,
+                       #[prop(into)]
+                       href:String,
+                       #[prop(into)]
+                       text:String) -> impl IntoView{
+    view!{cx, 
+        <a href={href} class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{text}</a>
     }
 }
